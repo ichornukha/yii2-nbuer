69d524f7c79f33cb934d7892491f96653eca35af
IChornuha <igor.chornuha@gmail.com>
2017-06-08 19:46:38 +0300

    Base class for currency rate data

---
+++ nbuer/CurrencyRate.php
@@ -0,0 +1,85 @@
+<?php
+declare(strict_types=1);
+
+namespace IChornuha\nbuer\NationalBankOfUkraineExchangeRates;
+
+
+class CurrencyRate
+{
+
+    /**
+     * Attribute names saved 'as is' in National Bank API response for easy saving procedure;
+     */
+
+    /*
+     * Currency code
+     */
+    private $r030;
+    /**
+     * Currency name
+     */
+    private $txt;
+    /**
+     * Exchange rate in UAH
+     */
+    private $rate;
+    /**
+     * Currency ISO code
+     */
+    private $cc;
+    /**
+     * Date
+     */
+    private $exchangedate;
+
+    public function __construct(array $data)
+    {
+        $this->r030 = (int)$data['r030'];
+        $this->txt = (string)$data['txt'];
+        $this->rate = (float)$data['rate'];
+        $this->cc = (string)$data['cc'];
+        $this->exchangedate = (string)$data['exchangedate'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurrencyDigitalCode()
+    {
+        return $this->r030;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->txt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRate()
+    {
+        return $this->rate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getISOCode()
+    {
+        return $this->cc;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->exchangedate;
+    }
+
+
+}
